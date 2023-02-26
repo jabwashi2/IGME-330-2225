d408229619d80703c5a53bf8454daf352a77d859
Jabrecia Washington <jlw4958@g.rit.edu>
2023-02-26 02:08:36 -0500

    play button and volume slider functional

--- web-audio-hw/src/main.js
+++ web-audio-hw/src/main.js
@@ -8,6 +8,7 @@
 // If you want to re-write these as ES6 arrow functions, to be consistent with the other files, go ahead!
 
 import * as utils from './utils.js';
+import * as audio from './audio.js';
 
 // 1 - here we are faking an enumeration
 const DEFAULTS = Object.freeze({
@@ -15,6 +16,7 @@ const DEFAULTS = Object.freeze({
 });
 
 function init(){
+    audio.setupWebaudio(DEFAULTS.sound1);
 	console.log("init called");
 	console.log(`Testing utils.getRandomColor() import: ${utils.getRandomColor()}`);
 	let canvasElement = document.querySelector("canvas"); // hookup <canvas> element
@@ -30,6 +32,43 @@ function setupUI(canvasElement){
     console.log("init called");
     utils.goFullscreen(canvasElement);
   };
+
+  // add .onclick event to button
+  playButton.onclick = e =>{
+    console.log(`audioCtx.state before = ${audio.audioCtx.state}`);
+    
+    //check if contedxt is in suspended state (autoplay policy)
+    if (audio.audioCtx.state == "suspended"){
+        audio.audioCtx.resume();
+    }
+    console.log(`audioCtx.state after = ${audio.audioCtx.state}`);
+
+    if (e.target.dataset.playing == "no"){
+        // if track is currently paused, play it
+        audio.playCurrentSound();
+        e.target.dataset.playing = "yes"; // our CSS will set the text to 'Pause'
+        // if the track IS playing, pause it
+    }
+    else{
+        audio.pauseCurrentSound();
+        e.target.dataset.playing = "no"; // our CSS will set the text to 'Play'
+    }
+  };
+
+  // C - hookup volume slider & label
+  let volumeSlider = document.querySelector("#volumeSlider");
+  let volumeLabel = document.querySelector("#volumeLabel");
+
+  // add .oninput event to slider
+  volumeSlider.oninput = e =>{
+    // set the gain
+    audio.setVolume(e.target.value);
+    // update value of label to match value of slider
+    volumeLabel.innerHTML = Math.round((e.target.value/2 * 100));
+  }
+
+  // set value of label to match initial value of slider
+  volumeSlider.dispatchEvent(new Event("input"));
 	
 } // end setupUI
 
